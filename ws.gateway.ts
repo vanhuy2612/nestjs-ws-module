26218daab64180e518a5a6be25776890265e4690
HuyDV <huydv3@rikkeisoft.com>
2024-01-08 22:28:58 +0700

    update

--- ws.gateway.ts
+++ ws.gateway.ts
@@ -19,10 +19,13 @@ export class WSGateway {
   @WebSocketServer() server: Server;
   @Inject() private wsService: WSService;
 
-  handleConnection(socket: Socket) {}
+  handleConnection(socket: Socket) {
+    console.log(`client connected ${socket.id}`)
+  }
 
   @SubscribeMessage("ping")
   createRoom(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+    console.log(client.id, " ping");
     client.join("global-room");
     this.server.emit('pong', { joinRoom: true});
   }
